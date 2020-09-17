adbf93dfee168136fb836adb2177efe2c97860eb
kkatoch89 <karankatoch.hba@gmail.com>
2020-09-16 20:55:41 -0400

    Kinda working!

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
-const quickExg = {}
+const quickExg = {};
 
 quickExg.apiKey = 'c264a6564be08eac4e720222e86b3b61';
 quickExg.apiList = 'http://api.coinlayer.com/api/list';
 quickExg.apiLive = 'http://api.coinlayer.com/api/live';
 
-quickExg.listData = $.ajax({
-    url: quickExg.apiList,
-    method: 'GET',
-    dataType: 'JSON',
-    data: {
-        access_key: quickExg.apiKey,
-    }
-}).then((data) => {
-    console.log(data)
-})
+quickExg.getInput = () => {
+	$('#userCurrency').on('change', function () {
+		const selection = $(this).val();
+		console.log(selection);
+		quickExg.listData(selection);
+	});
+};
+
+// const userInput = quickExg.getInput();
+// console.log(userInput);
+
+// quickExg.displayInput;
+
+quickExg.listData = (input) => {
+	$.ajax({
+		url: quickExg.apiLive,
+		method: 'GET',
+		dataType: 'JSON',
+		data: {
+			access_key: quickExg.apiKey,
+			target: input,
+		},
+	}).then((data) => {
+		console.log(data);
+		// console.log(data.fiat[input]);
+		// console.log(data.crypto[input].name);
+	});
+};
+
+// quickExg.listData('USD');
+
+$(function () {
+	quickExg.getInput();
+});
